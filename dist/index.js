5363baa6d54faa8ad1fa00102ea652699c1e611f
gaddafi <gaddafiadamu400@gmail.com>
2024-04-14 21:56:15 +0100

    Optional element access operator

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,8 @@
 "use strict";
+var _a;
+const groceries = {
+    name: '',
+};
 let employee = {
     id: 1,
     name: 'Gaddafi',
@@ -15,4 +19,27 @@ function kgToLbs(weight) {
 }
 console.log(kgToLbs(10));
 console.log(kgToLbs('10'));
+let textBox = {
+    drag: () => { },
+    resize: () => { },
+};
+let quantity = 50;
+function greet(name) {
+    if (name)
+        console.log('Hoila!');
+    else
+        console.log('Not possible to greet');
+}
+greet(null);
+function getCustomer(id) {
+    return id === 0 ? null : { birthday: new Date() };
+}
+let customer = getCustomer(10);
+if (customer !== null)
+    console.log(customer.birthday);
+console.log((_a = customer === null || customer === void 0 ? void 0 : customer.birthday) === null || _a === void 0 ? void 0 : _a.getFullYear());
+let log = null;
+log === null || log === void 0 ? void 0 : log('a');
+let cakes = [0, 10];
+console.log(cakes === null || cakes === void 0 ? void 0 : cakes[0]);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
 // employee
 
+const groceries: {
+  name: string;
+} = {
+  name: '',
+};
+
 type Employee = {
   readonly id: number;
   name: string;
@@ -39,3 +45,38 @@ let textBox: UWidgets = {
   drag: () => {},
   resize: () => {},
 };
+
+type Quantity = 100 | 50;
+let quantity: Quantity = 50;
+
+function greet(name: string | null | undefined) {
+  if (name) console.log('Hoila!');
+  else console.log('Not possible to greet');
+}
+
+greet(null);
+
+// +++++++++++++++++++
+type Customer = {
+  birthday: Date;
+};
+
+function getCustomer(id: number): Customer | null {
+  return id === 0 ? null : { birthday: new Date() };
+}
+
+let customer = getCustomer(10);
+if (customer !== null) console.log(customer.birthday);
+
+// Optional property access operator
+console.log(customer?.birthday?.getFullYear());
+
+// Optional element access operator
+// customers?. [0]
+// Optional call
+let log: any = null;
+log?.('a');
+
+
+let cakes = [0, 10]
+console.log(cakes?.[0])
